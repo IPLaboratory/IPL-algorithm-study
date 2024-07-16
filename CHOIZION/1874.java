a0b7df9b48581bd99ec81ffc3ba070027bd6e31c
CHOIZION <128041768+CHOIZION@users.noreply.github.com>
2024-07-16 17:34:03 +0900

    Update 1874.java

--- CHOIZION/1874.java
+++ CHOIZION/1874.java
@@ -15,14 +15,13 @@ public class Main {
         while(N --> 0) {
             int value = Integer.parseInt(br.readLine());
             if(value > idx) {
-                // 1부터 value까지 push
+
                 for (int i = idx+1; i <= value; i++) {
                     stack.push(i);
                     sb.append('+').append('\n');
                 }
-                idx = value; // 중간 저장
+                idx = value;
 
-            // top에 있는 원소가 입력받은 값과 같이 않을 때
             }else if(stack.peek() != value) {
                 System.out.println("NO");
                 return;
